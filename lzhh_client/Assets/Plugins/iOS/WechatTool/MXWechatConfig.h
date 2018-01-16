d1f15fd2d3fdb1d08b1c9b73b36a20590716dd91
qianqians <anxiangqq@gmail.com>
2018-01-16 18:42:21 +0800

    Signed-off-by: qianqians <anxiangqq@gmail.com>

--- lzhh_client/Assets/Plugins/iOS/WechatTool/MXWechatConfig.h
+++ lzhh_client/Assets/Plugins/iOS/WechatTool/MXWechatConfig.h
@@ -14,14 +14,14 @@
  */
 
 // 开放平台登录https://open.weixin.qq.com的开发者中心获取APPID
-#define MXWechatAPPID       @"wxafd936830b3cf60f"
+#define MXWechatAPPID       @"xxxxxxxxx"
 // 开放平台登录https://open.weixin.qq.com的开发者中心获取AppSecret。
-#define MXWechatAPPSecret   @"e72afcd7738d3fb3c6a6600f7a8f299f"
+#define MXWechatAPPSecret   @"xxxxxxxxxxxxxxxxxxxxxxxx"
 // 微信支付商户号
-#define MXWechatMCHID       @"1449873902"
+#define MXWechatMCHID       @"xxxxxxxxx"
 // 安全校验码（MD5）密钥，商户平台登录账户和密码登录http://pay.weixin.qq.com
 // 平台设置的“API密钥”，为了安全，请设置为以数字和字母组成的32字符串。
-#define MXWechatPartnerKey  @"sanyuanhy15618525339l88888888888"
+#define MXWechatPartnerKey  @"xxxxxxxxxxxxxxxxxxxxxxxx"
 
 
 /**
